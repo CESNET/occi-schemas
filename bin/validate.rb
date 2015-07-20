774b802cd25a4233df6f197af5d2024d9bedab00
Zdenek Sustr <git@sustr.net>
2015-07-20 16:30:58 +0200

    Align enumeration format with validator output.

--- bin/validate.rb
+++ bin/validate.rb
@@ -5,7 +5,7 @@ require 'json-schema'
 out = JSON::Validator.fully_validate(ARGV[0], ARGV[1])
 
 iter=0
-out.each { |msg| puts "#{iter += 1}.\t#{msg}\n" }
+out.each { |msg| puts "\##{iter += 1}:\t#{msg}\n" }
 
 out.count
 
