ce7e6bca9355aef38317e0ec89617536dc032282
Zdenek Sustr <git@sustr.net>
2015-07-21 10:24:35 +0200

    Making the validator cleverer and talkier

--- bin/validate.rb
+++ bin/validate.rb
@@ -2,10 +2,15 @@
 
 require 'json-schema'
 
-out = JSON::Validator.fully_validate(ARGV[0], ARGV[1])
+if (ARGV.count == 2 and File.file?(ARGV[0]) and File.file?(ARGV[1]))
+  out = JSON::Validator.fully_validate(ARGV[0], ARGV[1])
 
-iter=0
-out.each { |msg| puts "\##{iter += 1}:\t#{msg}\n" }
-
-out.count
+  iter=0
+  out.each { |msg| puts "\##{iter += 1}:\t#{msg}\n" }
+  
+  out.count
+else
+  puts "Usage:\n\n#{File.basename($PROGRAM_NAME)} <schema.json> <data.json>\n\nThe 'json-schema' gem is used to validate the data.\n\n"
+  -1
+end
 
